a6e5183a359714d0aa45cb23e144167074a24f88
Miguel Fazio de Assunção <faziomiguel0@gmail.com>
2025-07-16 07:48:21 -0300

    feat: criação das classes

---
+++ script.js
@@ -0,0 +1,27 @@
+class Passageiro {
+    constructor(nome, documento, statusEmbarque, bagagemRegistrada) {
+        this.nome = nome;
+        this.documento = documento;
+        this.statusEmbarque = statusEmbarque;
+        this.bagagemRegistrada = bagagemRegistrada;
+    }
+}
+
+class Voo {
+    constructor(numero, destino, horario, capacidade, quantidadeEmbarcados) {
+        this.numero = numero;
+        this.destino = destino;
+        this.horario = horario;
+        this.capacidade = capacidade;
+        this.quantidadeEmbarcados = quantidadeEmbarcados;
+    }
+}
+
+class Bagagem {
+    constructor(peso, tipo, autorizada) {
+        this.peso = peso;
+        this.tipo = tipo;
+        this.autorizada = autorizada;
+    }
+}
+
